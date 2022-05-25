c0024dd17854cbc291b99b0595ecc33a18074c26
sergiomerayo <sergiomerayo@gmail.com>
2022-05-25 23:23:06 +0200

    TFM-31 Added option to add comment to employee

--- src/app/shared/services/employee.service.ts
+++ src/app/shared/services/employee.service.ts
@@ -13,15 +13,15 @@ export class EmployeeService {
   constructor(private httpService: HttpService) { }
 
   search(employeeSearch: EmployeeSearch): Observable<Employee[]> {
-    return of([{identifier:"1", salary:100000, category:"senior", name:"sergio"},
-      {identifier:"1", salary:100000, category:"senior", name:"sergio"},
-      {identifier:"1", salary:100000, category:"senior", name:"sergio"},
-      {identifier:"1", salary:100000, category:"senior", name:"sergio"}
+    return of([{identifier:"1", salary:100000, category:"senior", name:"sergio", comments:["asdasd", "fafdasdafsaf"]},
+      {identifier:"2", salary:100000, category:"senior", name:"sergio", comments:["hola k ase,", "jajaja"]},
+      {identifier:"1", salary:100000, category:"senior", name:"sergio", comments:[]},
+      {identifier:"1", salary:100000, category:"senior", name:"sergio", comments:[]}
     ]);
   }
 
   read(name: string) {
-    return of({identifier:"2", salary:200000, category:"junior", name:"carlos"});
+    return of({identifier:"2", salary:200000, category:"junior", name:"carlos", comments: ["hola k ase,", "jajaja","asdasd", "fafdasdafsaf","asdasd", "fafdasdafsaf","asdasd", "fafdasdafsaf","asdasd", "fafdasdafsaf","asdasd", "fafdasdafsaf","asdasd", "fafdasdafsaf","asdasd", "fafdasdafsaf","asdasd", "fafdasdafsaf","asdasd", "fafdasdafsaf","asdasd", "fafdasdafsaf","asdasd", "fafdasdafsaf"]});
   }
 
   create(employee: Employee) {
