0acff270cf3488606f38ce0e91dab896ea4ab65d
sergiomerayo <sergiomerayo@gmail.com>
2022-05-28 15:28:35 +0200

    TFM fixes

--- src/app/home/report-feedback/report-feedback.component.ts
+++ src/app/home/report-feedback/report-feedback.component.ts
@@ -38,6 +38,7 @@ export class ReportFeedbackComponent implements OnInit {
   }
 
   reset() {
+    this.feedback = {identifier: "", feedback: ""};
     this.feedbacks = this.reportFeedbackService.search();
   }
 
